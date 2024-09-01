36d23e32b6a56d49c57d2446a4668d6c8f786bb0
AbhayAprosoft <abhay@aprosoftech.com>
2024-09-01 14:46:52 +0530

    Feat : General : New command for truncate single table

---
+++ app/Console/Commands/TruncateTable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class TruncateTable extends Command
+{
+    // The name and signature of the console command.
+    protected $signature = 'db:truncate {table}';
+
+    // The console command description.
+    protected $description = 'Truncate a specified database table';
+
+    // Create a new command instance.
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    // Execute the console command.
+    public function handle()
+    {
+        $table = $this->argument('table');
+        
+        if (!Schema::hasTable($table)) {
+            $this->error("Table '{$table}' does not exist.");
+            return 1; // Error code
+        }
+        
+        DB::table($table)->truncate();
+        $this->info("Table '{$table}' has been truncated.");
+        return 0; // Success code
+    }
+}
