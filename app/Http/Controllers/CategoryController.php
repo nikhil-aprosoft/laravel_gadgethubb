3b03bb9576b3e7793c2149f1b4ee71e60466a760
AbhayAprosoft <abhay@aprosoftech.com>
2024-09-26 21:44:35 +0530

    Refl: website : shoes server cat id added

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -25,7 +25,7 @@ class CategoryController extends Controller
         $dailyDeals = DailyDeal::With('product.attributes')->latest()->get();
         $bestSeller = $this->bestSeller();
         $newArrival = Product::latest()->limit(10)->get();
-        $shoesSection = Product::where('category_id', '=', "1449b012-f7d7-4004-a08f-aa227390bed3")->limit(8)->get();
+        $shoesSection = Product::where('category_id', '=', "841922dd-11b7-460f-95ff-2f3b0dc2de2a")->limit(8)->get();
         return view('website.index', compact('categories', 'parentCategoriesMega', 'parentCategoriesNormal', 'banners', 'featureBanners', 'bestSeller', 'dailyDeals', 'newArrival', 'shoesSection'));
     }
     public function banners()
