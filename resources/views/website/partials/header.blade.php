5cb72521ac89707baca7f33d22827fa6da4cc3e2
AbhayAprosoft <abhay@aprosoftech.com>
2024-09-28 11:41:07 +0530

    Ref: Website : Phone no added in header

--- resources/views/website/partials/header.blade.php
+++ resources/views/website/partials/header.blade.php
@@ -52,12 +52,8 @@
             <div class="header-right ml-4">
                 <div class="header-call d-xs-show d-lg-flex align-items-center">
                     <a href="tel:#" class="w-icon-call"></a>
-                    <div class="call-info d-lg-show">
-                        <h4 class="chat font-weight-normal font-size-md text-normal ls-normal text-light mb-0">
-                            <a href="https://portotheme.com/cdn-cgi/l/email-protection#3a19"
-                                class="text-capitalize">Live Chat</a> or :
-                        </h4>
-                        <a href="tel:#" class="phone-number font-weight-bolder ls-50">0(800)123-456</a>
+                    <div class="call-info d-lg-show">                   
+                        <a href="tel:+91 8699643223" class="phone-number font-weight-bolder ls-50">+91 8699643223</a>
                     </div>
                 </div>
                 <a class="wishlist label-down link d-xs-show" href="wishlist.html">
