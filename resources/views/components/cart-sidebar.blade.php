8fbcf003fefbeab56acfff42eda3002fa7b1c64d
AbhayAprosoft <abhay@aprosoftech.com>
2024-09-29 13:31:24 +0530

    Feat : Header : cart sidebar

--- resources/views/components/cart-sidebar.blade.php
+++ resources/views/components/cart-sidebar.blade.php
@@ -3,14 +3,9 @@
     <a href="#" class="cart-toggle label-down link">
         <i class="w-icon-cart">
             @php
-            $userId = session('user')->userid ?? null;
-
-            if ($userId) {
-                $carCount = \Cart::where('user_id', $userId)->count();
-            } else {
-                $carCount = 0;
-            }
-        @endphp
+                $userId = session('user')->userid ?? null;
+                $carCount = $userId ? Cart::where('user_id', $userId)->count() : 0;
+            @endphp
             <span class="cart-count">{{ $carCount }}</span>
         </i>
         <span class="cart-label">Cart</span>
@@ -22,57 +17,47 @@
         </div>
 
         <div class="products">
-            <div class="product product-cart">
-                <div class="product-detail">
-                    <a href="product-default.html" class="product-name">Beige knitted
-                        elas<br>tic
-                        runner shoes</a>
-                    <div class="price-box">
-                        <span class="product-quantity">1</span>
-                        <span class="product-price">$25.68</span>
-                    </div>
-                </div>
-                <figure class="product-media">
-                    <a href="product-default.html">
-                        <img src="assets/images/cart/product-1.jpg" alt="product" height="84"
-                            width="94" />
-                    </a>
-                </figure>
-                <button class="btn btn-link btn-close" aria-label="button">
-                    <i class="fas fa-times"></i>
-                </button>
-            </div>
+            @php
+                $cartItems = $userId ? Cart::with('product')->where('user_id', $userId)->get() : collect();
+            @endphp
 
-            <div class="product product-cart">
-                <div class="product-detail">
-                    <a href="product-default.html" class="product-name">Blue utility
-                        pina<br>fore
-                        denim dress</a>
-                    <div class="price-box">
-                        <span class="product-quantity">1</span>
-                        <span class="product-price">$32.99</span>
+            @foreach ($cartItems as $item)
+                <div class="product product-cart">
+                    <div class="product-detail">
+                        <a href="{{ route('product.show', $item->product->id) }}"
+                            class="product-name">{{ $item->product->name }}</a>
+                        <div class="price-box">
+                            <span class="product-quantity">{{ $item->quantity }}</span>
+                            <span class="product-price">${{ number_format($item->product->price, 2) }}</span>
+                        </div>
                     </div>
+                    <figure class="product-media">
+                        <a href="{{ route('product.show', $item->product->id) }}">
+                            <img src="{{ asset('assets/images/cart/' . $item->product->image) }}"
+                                alt="{{ $item->product->name }}" height="84" width="94" />
+                        </a>
+                    </figure>
+                    <button class="btn btn-link btn-close" aria-label="button" data-id="{{ $item->id }}">
+                        <i class="fas fa-times"></i>
+                    </button>
                 </div>
-                <figure class="product-media">
-                    <a href="product-default.html">
-                        <img src="assets/images/cart/product-2.jpg" alt="product" width="84"
-                            height="94" />
-                    </a>
-                </figure>
-                <button class="btn btn-link btn-close" aria-label="button">
-                    <i class="fas fa-times"></i>
-                </button>
-            </div>
+            @endforeach
         </div>
 
         <div class="cart-total">
             <label>Subtotal:</label>
-            <span class="price">$58.67</span>
+            <span
+                class="price">${{ number_format(
+                    $cartItems->sum(function ($item) {
+                        return $item->product->price * $item->quantity;
+                    }),
+                    2,
+                ) }}</span>
         </div>
 
         <div class="cart-action">
-            <a href="cart.html" class="btn btn-dark btn-outline btn-rounded">View Cart</a>
-            <a href="checkout.html" class="btn btn-primary  btn-rounded">Checkout</a>
+            <a href="{{ route('cart.index') }}" class="btn btn-dark btn-outline btn-rounded">View Cart</a>
+            <a href="{{ route('checkout.index') }}" class="btn btn-primary btn-rounded">Checkout</a>
         </div>
     </div>
     <!-- End of Dropdown Box -->
